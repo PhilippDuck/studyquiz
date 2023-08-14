c31bee5e00bb830da0a01087d3c6e0aabe41577f
Philipp Duck <phil.mar.du@gmail.com>
2023-08-14 23:33:31 +0200

    zwischenstand

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -42,10 +42,35 @@ function Create() {
         <CardBody>
           
             <Accordion  allowMultiple>
-              <AccordionItem>
+              <AccordionItem border="none">
                 <AccordionButton>
                  
-                  <Heading size="xs">Was ist der Unterschied zwischen RAM und ROM?</Heading>
+                  <Text size="xs">Was ist der Unterschied zwischen RAM und ROM?</Text>
+                  <AccordionIcon />
+                </AccordionButton>
+
+                <AccordionPanel pb={4}>
+                  <ul>
+                    <li>1. Test</li>
+                    <li>2. Test</li>
+                    <li>3. Test</li>
+                    <li>4. Test</li>
+                  </ul>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          
+        </CardBody>
+      </Card>
+      <Box h="10px"></Box>
+      <Card w="100%">
+        <CardBody>
+          
+            <Accordion  allowMultiple>
+              <AccordionItem border="none">
+                <AccordionButton>
+                 
+                  <Text size="xs">Was ist der Unterschied zwischen RAM und ROM?</Text>
                   <AccordionIcon />
                 </AccordionButton>
 
