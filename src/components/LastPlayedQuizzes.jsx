653aedf37d42fb3107412b1b502917bd5425f728
Philipp Duck <phil.mar.du@gmail.com>
2023-08-29 13:52:36 +0200

    Quiznamen in Übersicht letzte Spiele

--- src/components/LastPlayedQuizzes.jsx
+++ src/components/LastPlayedQuizzes.jsx
@@ -1,45 +1,48 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 import { useRealm } from "../provider/RealmProvider";
-import { VStack, Heading, Box } from '@chakra-ui/react';
-import PlayedQuizCard from './playedQuizCard';
+import { VStack, Heading, Box, Spinner, Center } from "@chakra-ui/react";
+import PlayedQuizCard from "./playedQuizCard";
 
 function LastPlayedQuizzes() {
+  const app = useRealm();
+  const [lastPlayedQuizzes, setLastPlayedQuizzes] = useState([]);
+  const [isLoadingLastPlayedQuizzes, setIsLoadingLastPlayedQuizzes] = useState(true);
 
-    const app = useRealm();
-    const [lastPlayedQuizzes, setLastPlayedQuizzes] = useState([]);
+  useEffect(() => {
+    setIsLoadingLastPlayedQuizzes(true);
+    getLastPlayedQuizzes(5);
+    setIsLoadingLastPlayedQuizzes(false);
+  }, []);
 
-    useEffect(()=> {
-        getLastPlayedQuizzes(5);
-    }, []);
-
-    async function getLastPlayedQuizzes(numberOfQuizzes) {
-
-        const result = await app.currentUser.functions.getLastPlayedQuizzes(numberOfQuizzes);
-        setLastPlayedQuizzes(result);
-        console.log(result);
-        return result;
-
-      }
+  async function getLastPlayedQuizzes(numberOfQuizzes) {
+    const result = await app.currentUser.functions.getLastPlayedQuizzes(
+      numberOfQuizzes
+    );
+    setLastPlayedQuizzes(result);
+    console.log(result);
+    return result;
+  }
 
   return (
     <>
-    <Heading size={"lg"}>Letzte Spiele</Heading>
-    <Box h="30px"></Box>
-        <VStack>
-                      {lastPlayedQuizzes.map((playedQuiz) => {
-                        return (
-                          <PlayedQuizCard
-                            key={playedQuiz._id}
-                            playedQuiz={playedQuiz}
-                            withQuizName={true}
-                          />
-                        );
-                      })}
-
-                    </VStack> 
+      <Heading size={"lg"}>Letzte Spiele</Heading>
+      <Box h="30px"></Box>
+      
+      {isLoadingLastPlayedQuizzes ? <Center><Spinner size={"xl"}/></Center> : <VStack>
+        {lastPlayedQuizzes.map((playedQuiz) => {
+          return (
+            <PlayedQuizCard
+              key={playedQuiz._id}
+              playedQuiz={playedQuiz}
+              withQuizName={true}
+            />
+          );
+        })}
+      </VStack>}
+      
     </>
-  )
+  );
 }
 
-export default LastPlayedQuizzes
+export default LastPlayedQuizzes;
--- src/components/PlayedQuizCard.jsx
+++ src/components/PlayedQuizCard.jsx
@@ -1,55 +1,88 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {Card, Text, CardHeader, Avatar, Flex, Box, Center, Tooltip } from "@chakra-ui/react";
+import {
+  Card,
+  Text,
+  CardHeader,
+  Avatar,
+  Flex,
+  Box,
+  Center,
+  Tooltip,
+} from "@chakra-ui/react";
 import { useRealm } from "../provider/RealmProvider";
 
-
 function PlayedQuizCard(props) {
-    const [owner, setOwner] = useState("");
-    const app = useRealm();
+  const [owner, setOwner] = useState("");
+  const app = useRealm();
 
-    function unixToReadableDate(unixTimestamp) {
-        // Erstellt ein neues Date-Objekt basierend auf dem Unix-Timestamp (multipliziert mit 1000, da JavaScript Millisekunden erwartet)
-        const date = new Date(unixTimestamp);
-      
-        // Formatierung des Datums und der Uhrzeit
-        const formattedDate = date.toLocaleDateString(); // z.B. "26.08.2023"
-        const formattedTime = date.toLocaleTimeString(); // z.B. "12:34:56"
-      
-        return `${formattedDate} ${formattedTime}`;
+  function unixToReadableDate(unixTimestamp) {
+    // Erstellt ein neues Date-Objekt basierend auf dem Unix-Timestamp (multipliziert mit 1000, da JavaScript Millisekunden erwartet)
+    const date = new Date(unixTimestamp);
+
+    // Formatierung des Datums und der Uhrzeit
+    const formattedDate = date.toLocaleDateString(); // z.B. "26.08.2023"
+    const formattedTime = date.toLocaleTimeString(); // z.B. "12:34:56"
+
+    return `${formattedDate} ${formattedTime}`;
+  }
+
+  useEffect(() => {
+    async function getNicknameById() {
+      if (app.currentUser) {
+        const result = await app.currentUser.functions.getNicknameById({
+          id: props.playedQuiz.playerId,
+        }); // Ersetzen Sie 'IhreFunktionName' durch den Namen Ihrer Funktion
+        //console.log('Ergebnis der Funktion:', result);
+        return result;
+      } else {
+        console.log("errr");
       }
-    
-
-    useEffect(() => {
-        async function getNicknameById() {
-          if (app.currentUser) {
-            const result = await app.currentUser.functions.getNicknameById({ id: props.playedQuiz.playerId }); // Ersetzen Sie 'IhreFunktionName' durch den Namen Ihrer Funktion
-            //console.log('Ergebnis der Funktion:', result);
-            return result;
-          } else {
-            console.log("errr");
-          }
-    
-        }
-        getNicknameById().then(e => {
-          setOwner(e);
-        });
-      }, [])
+    }
+    getNicknameById().then((e) => {
+      setOwner(e);
+    });
+  }, []);
 
   return (
-    <Card variant={"outline"} w={"100%"} >
+    <Card variant={"outline"} w={"100%"}>
       <CardHeader>
-        <Flex gap={"20px"}><Center><Tooltip label={owner}><Avatar name={owner} /></Tooltip></Center>
-        <Box>
-        <Text fontSize={"xs"}>{unixToReadableDate(props.playedQuiz.endTime)} Uhr </Text>
-        {props.withQuizName ?<Text fontSize={"xs"}><b>{owner}</b>{" hat das Quiz " + props.playedQuiz._id + " gespielt."} </Text> : <b><Text>{owner}</Text></b>}
-        <Text fontSize={"xs"}>
-          {props.playedQuiz.points}{" "}
-          {props.playedQuiz.points === 1 || props.playedQuiz.points === -1
-            ? "Punkt"
-            : "Punkte"} in {props.playedQuiz.playedTime} Sekunden erreicht.
-        </Text>
-        </Box>
+        <Flex gap={"20px"}>
+          <Center>
+            <Tooltip label={owner}>
+              <Avatar name={owner} />
+            </Tooltip>
+          </Center>
+          <Box>
+            <Text fontSize={"xs"}>
+              {unixToReadableDate(props.playedQuiz.endTime)} Uhr{" "}
+            </Text>
+            {props.withQuizName ? (
+              <Text fontSize={"xs"}>
+                <b>{owner}</b>
+                {" hat das Quiz "} <b>{props.playedQuiz.quizTitle}</b>{" "}
+                {" gespielt und dabei"} {props.playedQuiz.points}{" "}
+                {props.playedQuiz.points === 1 || props.playedQuiz.points === -1
+                  ? "Punkt"
+                  : "Punkte"}{" "}
+                in {props.playedQuiz.playedTime} Sekunden erreicht.
+              </Text>
+            ) : (
+              <>
+                <b>
+                  <Text>{owner}</Text>
+                </b>
+                <Text fontSize={"xs"}>
+                  {props.playedQuiz.points}{" "}
+                  {props.playedQuiz.points === 1 ||
+                  props.playedQuiz.points === -1
+                    ? "Punkt"
+                    : "Punkte"}{" "}
+                  in {props.playedQuiz.playedTime} Sekunden erreicht.
+                </Text>
+              </>
+            )}
+          </Box>
         </Flex>
       </CardHeader>
     </Card>
