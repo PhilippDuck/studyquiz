7f96d33291f254eff4f0e7a0b88bb7cef05c3923
Philipp Duck <phil.mar.du@gmail.com>
2023-08-30 22:22:53 +0200

    Highscoreseite, Highscore komponente

---
+++ src/pages/Highscores.jsx
@@ -0,0 +1,47 @@
+import { Container, Select, Box } from "@chakra-ui/react";
+import React from "react";
+import HighscoreTable from "../components/HighscoreTable";
+import { useEffect, useState } from "react";
+import { useRealm } from "../provider/RealmProvider";
+
+function Highscores() {
+  const app = useRealm();
+  const [quizzes, setQuizzes] = useState([]);
+  const [selectedQuiz, setSelectedQuiz] = useState("");
+
+  useEffect(() => {
+    async function getQuizzes() {
+      if (app.currentUser) {
+        const result = await app.currentUser.functions.getQuizzes(); // Ersetzen Sie 'IhreFunktionName' durch den Namen Ihrer Funktion
+        //console.log('Ergebnis der Funktion:', result);
+        setQuizzes(result);
+      } else {
+        console.log("nicht eingeloggt");
+      }
+    }
+    getQuizzes();
+  }, []);
+
+  return (
+    <Container>
+      <Select
+        id="quiz"
+        name="quiz"
+        onChange={(e) => {
+          setSelectedQuiz(e.target.value);
+        }}
+      >
+        <option value="">Wählen ein Quiz</option>
+        {quizzes.map((q, index) => (
+          <option value={q._id} key={index}>
+            {q.title}
+          </option>
+        ))}
+      </Select>
+      <Box h={"30px"} />
+      <HighscoreTable quizId={selectedQuiz} />
+    </Container>
+  );
+}
+
+export default Highscores;
